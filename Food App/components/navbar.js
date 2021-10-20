e50218a475fdb090c07b32818d9abfa0ca0e434c
ipabitrasaikia1 <79037063+ipabitrasaikia1@users.noreply.github.com>
2021-10-20 14:44:12 +0530

    Update navbar.js

--- Food App/components/navbar.js
+++ Food App/components/navbar.js
@@ -6,8 +6,8 @@ function navbar() {
     <div id="Options">
     
        
-        <h3><a href="reciepeOftheday.html">Reciepe Of The Day</a></h3>
-        <h3><a href="latestReciepe.html">Latest Reciepe.</a></h3>
+        <h3><a href="reciepeOftheday.html">Recipe Of The Day</a></h3>
+        <h3><a href="latestReciepe.html">Latest Recipe.</a></h3>
         <h3><a href="#">Signin</a></h3>
         <h3><a href="#">Login</a></h3>
 
